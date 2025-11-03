30dc0454295d71c905bf47b7372b302b9b5fe0ed
HoangDinh <hoangdinh.040104@gmail.com>
2025-11-04 04:15:04 +0700

    Updated

--- todolist-backend/src/routes/auth.route.js
+++ todolist-backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { protect } from "../middleware/auth.middleware.js";
 import { register, login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", protect, register);
+router.post("/login", protect, login);
 
 export default router;
