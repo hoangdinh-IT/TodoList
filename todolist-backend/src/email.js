8600bf44cbd54e26dd9a6c06926096440c7d5b81
HoangDinh <hoangdinh.040104@gmail.com>
2025-11-06 15:41:19 +0700

    Updated

--- todolist-backend/src/email.js
+++ todolist-backend/src/email.js
@@ -2,14 +2,22 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT) || 456,
-    secure: process.env.EMAIL_SECURE === "true",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+// const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: Number(process.env.EMAIL_PORT) || 456,
+//     secure: process.env.EMAIL_SECURE === "true",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER, // Gmail của bạn
+    pass: process.env.EMAIL_PASS, // App Password
+  },
 });
 
 const verifyTransporter = async () => {
