13ed76220fda3af3f7260a7d4d36e6c7a2cc4253
HoangDinh <hoangdinh.040104@gmail.com>
2025-11-06 16:04:30 +0700

    Updated

---
+++ todolist-backend/src/testEmail.js
@@ -0,0 +1,17 @@
+// src/testEmail.js
+import dotenv from "dotenv";
+dotenv.config();
+
+import { verifyTransporter } from "./email.js";
+
+verifyTransporter();
+
+// const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: Number(process.env.EMAIL_PORT) || 456,
+//     secure: process.env.EMAIL_SECURE === "true",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
