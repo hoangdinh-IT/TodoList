9c5227c2417ebb2c947f30a22025ec69bcf14736
HoangDinh <hoangdinh.040104@gmail.com>
2025-11-07 10:32:25 +0700

    Updated

--- todolist-backend/src/server.js
+++ todolist-backend/src/server.js
@@ -10,7 +10,8 @@ import categoryRoutes from "./routes/category.route.js";
 const app = express();
 
 const allowedOrigins = [
-  'https://todolist-rkait.vercel.app',  // URL FE Vercel của bạn
+  // 'https://todolist-rkait.vercel.app',  // URL FE Vercel của bạn
+  'https://todolist-rkait.netlify.app',  // URL FE Vercel của bạn
   'http://localhost:5173'               // Local dev (Vite default port)
 ];
 
